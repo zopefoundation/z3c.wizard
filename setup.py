77013430b36d6d616d4e6703f1b90c94dcdf1518
Roger Ineichen <roger@projekt01.ch>
2009-02-22 06:03:49 +0000

    added dev marker

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.wizard',
-    version='0.5.0',
+    version='0.5.1dev',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "Wizard based on z3c.form for for Zope3",
