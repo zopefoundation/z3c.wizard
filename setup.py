0b646ccfe6740e1e1b60bcffae32bf40d59dd2cf
Michael Howitz <mh@gocept.com>
2011-10-28 21:07:56 +0000

    Back to development: 0.9.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.wizard',
-    version='0.9.1',
+    version='0.9.2dev',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "Wizard based on z3c.form for for Zope3",
