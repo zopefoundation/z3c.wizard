6a5e2482abb8b710919d3b023066a80866abc332
Jürgen Gmach <juergen.gmach@googlemail.com>
2020-11-08 10:34:47 +0100

    Add support for Python 3.9, drop support for 3.5
    This also fixes the broken `lint` environment.
    
    modified:   .travis.yml
    modified:   CHANGES.rst
    modified:   setup.py
    modified:   tox.ini

--- setup.py
+++ setup.py
@@ -43,10 +43,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Natural Language :: English',
@@ -66,6 +66,7 @@ setup(
         '!=3.2.*',
         '!=3.3.*',
         '!=3.4.*',
+        '!=3.5.*',
     ]),
     extras_require=dict(
         test=[
